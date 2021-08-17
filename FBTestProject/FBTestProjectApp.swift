a1b7166cbaf0649a065b39d47a15e6bb9d85208d
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-08-17 19:42:09 +0500

    Initial Commit

---
+++ FBTestProject/FBTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBTestProjectApp.swift
+//  FBTestProject
+//
+//  Created by Muhammad Khan on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct FBTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
